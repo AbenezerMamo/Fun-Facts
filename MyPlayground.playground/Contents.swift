3c7523750f2374b774fbe4e9d3f9fcebe05ed68f
Abenezer Mamo <ElitePowered@gmail.com>
2015-08-24 19:16:04 -0400

    Basic framework of the app.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var randomNumber = Int(arc4random_uniform(10))
+
+
+
+//UICOLor
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 95/255.0, alpha: 1.0)
+
+let colorsArray = [redColor]
